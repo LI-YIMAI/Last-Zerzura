b807a72da6d898c98754bf63a75ba2d719e5c29a
Chang Liu <changliu8@cmail.carleton.ca>
2020-03-26 17:03:25 -0400

    map updated

---
+++ Last Odyseey/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,294 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : Singleton<GameManager>
+{
+    
+    
+
+    public TowerBtn ClickedBtn { get; set; }
+
+    // property
+    public int Gold
+    {
+        get
+        {
+            return gold;
+        }
+        set
+        {
+            // update gold value for script and ui text 
+            this.gold = value;
+            this.Goldtext.text = value.ToString() + " <color=lime>$</color>"; 
+        }
+    }
+    // property
+    public bool WaveActive
+    {
+        get { return Activemonster.Count > 0; }
+         
+    }
+
+    private int gold;
+    private int wave = 0;
+    [SerializeField]
+    private Text WaveTxt; // this will have a referece for the actual waveTxt
+    [SerializeField]
+    private GameObject wavebtn;
+    [SerializeField]
+    private Text Goldtext;
+    [SerializeField]
+    private Text sellText;
+    [SerializeField]
+    private Text upgradeText;
+    private int health = 15;
+    private List<Monster> Activemonster = new List<Monster >();
+    [SerializeField]
+    private GameObject upgradePanel;
+
+    //current selected Tower
+    public Tower selectedTower;
+
+
+    public ObjectPool Pool { get; set; }
+
+    private void Awake()
+    {
+        Pool = GetComponent<ObjectPool>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set the Gold to 5 and assign it to Goldtext 
+        Gold = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleEscape();
+        shandow_tower_check();
+        if (selectedTower != null)
+        {
+            int extra_price = selectedTower.Count - 1;
+            sellText.text = "+ " + ((selectedTower.Price / 2) + extra_price).ToString();
+            upgradeText.text = "- " + (selectedTower.Price * selectedTower.Count).ToString();
+        }
+    }
+    public void PickTower(TowerBtn towerBtn)
+    {
+        
+        // check if have enough gold to buy a tower 
+        if (Gold >= towerBtn.Price && !WaveActive)
+        {
+            this.ClickedBtn = towerBtn;
+            Hover.Instance.Activate(towerBtn.Sprite);  
+        }
+        if (Gold == 0 || Gold < towerBtn.Price)
+        {
+            Hover.Instance.Deactivate();
+        }
+    }
+
+    public void Buytower()
+    {
+        // reduce the gold when placing the tower
+        
+        if (Gold >= ClickedBtn.Price)
+        {
+            Gold -= ClickedBtn.Price;
+            Hover.Instance.Deactivate();
+            //shandow_tower_check();
+        }
+        else
+        {
+            Hover.Instance.Deactivate();
+            //shandow_tower_check();
+        }   
+    }
+
+    public void HandleEscape()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Hover.Instance.Deactivate();
+
+        }
+    }
+
+    public void shandow_tower_check()
+    {
+        if (selectedTower != null)
+        {
+            GameObject upgrade = GameObject.FindGameObjectWithTag("UpgradeBtn");
+            Image img = upgrade.GetComponent<Image>();
+            Button btn = upgrade.GetComponent<Button>();
+            if (Gold == 0 || Gold < selectedTower.Price * selectedTower.Count)
+            {   
+                img.color = new Color32(176, 118, 118, 255);
+                btn.enabled = false;
+            }
+            else
+            {
+                img.color = Color.white;
+                btn.enabled = true;
+            }
+        }
+        
+        //Get gameobject for all button save as TowerBtn
+        //Check if the btn.price is greater than the current gold we have
+        //Yes -> shandow button
+        //Else no
+        TowerBtn Storm = GameObject.FindGameObjectWithTag("StormBtn").GetComponent<TowerBtn>();
+        TowerBtn Frozen = GameObject.FindGameObjectWithTag("FrozBtn").GetComponent<TowerBtn>();
+        //TowerBtn Poision = GameObject.FindGameObjectWithTag("PoisionBtn").GetComponent<TowerBtn>();
+//        TowerBtn Fire = GameObject.FindGameObjectWithTag("FireBtn").GetComponent<TowerBtn>();
+        if(Gold < 5)
+        {
+            //Poision.GetComponent<Image>().color = new Color32(185, 155, 155, 255);
+        }
+        else
+        {
+            //Poision.GetComponent<Image>().color = Color.white;
+        }
+        if (Gold < 3)
+        {
+            Frozen.GetComponent<Image>().color = new Color32(185, 155, 155, 255);
+//            Fire.GetComponent<Image>().color = new Color32(185, 155, 155, 255);
+        }
+        else
+        {
+            Frozen.GetComponent<Image>().color = Color.white;
+     //       Fire.GetComponent<Image>().color = Color.white;
+        }
+        if (Gold < 2)
+        {
+            Storm.GetComponent<Image>().color = new Color32(185, 155, 155, 255);
+        }
+        else
+        {
+            Storm.GetComponent<Image>().color = Color.white;
+        }
+        
+    }
+    
+
+    public void StartWave()
+    {
+        wave++;
+        WaveTxt.text = string.Format("Level: <color=lime>{0}</color>",wave);
+        // user clikc Next Wave button and activate this function to call SpawnWave
+        StartCoroutine(SpawnWave());
+        wavebtn.SetActive(false);
+    }
+
+    private IEnumerator SpawnWave()
+    {
+
+        // call GeneratePath function in LevelManger and assigned the path to the path property in LevelManager
+        LevelManager.Instance.GeneratePath();
+        // spawnwave based on wave number or level number 
+        for (int i = 0; i < wave; i++)
+        {
+            // random pick a index for MonsterPrefabs
+            // need to finish all animation for other monster, so far, we just use index 3 monster 
+            int monsterIndex = Random.Range(1, 4);
+            string type = string.Empty;
+            // according to the index to save the type of monster 
+            switch (monsterIndex)
+            {
+                case 0:
+                    type = "BlueMonster";
+                    break;
+                case 1:
+                    type = "GreenMonster";
+                    break;
+                case 2:
+                    type = "PurpleMonster";
+                    //type = "aba";
+                    break;
+                case 3:
+                    type = "RedMonster";
+                    //type = "aba";
+                    break;
+            }
+            // use the ObjectPool object to find the required type object and get its component Monster
+            Monster monster = Pool.GetObject(type).GetComponent<Monster>();
+            // call the spawn function in Monster script and set the Monster transform postion
+            monster.spawn(health);
+            // difficulty 
+            if (wave % 3 == 0)
+            {
+                health += 5;
+            }
+            // add active monster to the list 
+            Activemonster.Add(monster);
+
+            yield return new WaitForSeconds(2.5f);
+        }
+        
+    }
+    public void Removemonster(Monster monster)
+    {
+        Activemonster.Remove(monster);
+        // when there is no active monster, we need show the wave button 
+        if (!WaveActive)
+        {
+            wavebtn.SetActive(true);
+        }
+    }
+
+    public void SelectTower(Tower tower) {
+
+        if (selectedTower != null) {
+            selectedTower.Select();
+        }
+        selectedTower = tower;
+        selectedTower.Select();
+        //if selected, show up
+        //check if the tower has upgraded and calculate the sell price with upgrade
+       // int extra_price = selectedTower.Count - 1;
+        //sellText.text = "+ " + ((selectedTower.Price / 2)+extra_price).ToString();
+        //upgradeText.text = "- " + (selectedTower.Price * selectedTower.Count).ToString();
+        upgradePanel.SetActive(true);
+    }
+
+    public void DeselectTower() {
+
+        if (selectedTower != null) {
+            selectedTower.Select();
+        }
+        //if unselect, dispear 
+        upgradePanel.SetActive(false);
+        selectedTower = null;
+    }
+    public void SellTower()
+    {
+        if (selectedTower != null)
+        {
+            int extra_price = selectedTower.Count - 1;
+            //sell the 
+            Gold += ((selectedTower.Price / 2)+extra_price);
+            selectedTower.GetComponentInParent<TileScript>().IsEmpty = true;
+            Destroy(selectedTower.transform.parent.gameObject);
+            DeselectTower();
+        }
+    }
+    public void upgradeTower()
+    {
+        
+        if (selectedTower != null)
+        {
+            Debug.Log(Gold);
+            int num = selectedTower.Count;
+            Debug.Log(num);
+            Gold -= (selectedTower.Price * num);
+            Debug.Log(Gold);
+            selectedTower.Damage = selectedTower.Damage * selectedTower.Count;
+            selectedTower.Count++;
+        }
+    }
+
+}
---
+++ Last Odyseey/Assets/Scripts/Game/Monster.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Stack<Node> path;
+
+    public Point GridPosition { get; set; }
+
+    private Vector3 destination;
+    private SpriteRenderer spriteRenderer;
+    public bool IsActive { get; set; }
+    public bool Alive
+    {
+        get
+        {
+            return health.CurrentValue > 0;
+        }
+    }
+    private Animator myAnimator;
+    [SerializeField]
+    private Stat health;
+    private void Awake()
+    {
+        //sets up references to the components
+        myAnimator = GetComponent<Animator>();
+        health.Initialize();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        // been called per frame
+        Move();
+    }
+    public void spawn(int health)
+    {
+        // set the start spqwn position
+        transform.position = LevelManager.Instance.BluePortal.transform.position;
+        this.health.Bar.Reset();
+        this.health.MaxVal = health;
+        this.health.CurrentValue = this.health.MaxVal;
+        // get the animator component
+        //myAnimator = GetComponent<Animator>();
+        // starts to scale the monster 
+        StartCoroutine(Scale(new Vector3(0.1f, 0.1f), new Vector3(1, 1), false));
+        // once finished scaling the monster, we set the monster path which will be passed from LevelManager
+        SetPath(LevelManager.Instance.Path);
+    }
+
+    public IEnumerator Scale(Vector3 from, Vector3 to, bool remove)
+    {
+        //IsActive = false;
+        float progress = 0;
+
+        while (progress<=1)
+        {
+            //lerp is chaging the size over timel
+            transform.localScale = Vector3.Lerp(from, to, progress);
+
+            progress += Time.deltaTime;
+
+            yield return null;
+        }
+        transform.localScale = to;
+        IsActive = true;
+        if (remove)
+        {
+            Release();
+            
+        }
+    }
+
+    private void Move()
+    {
+        // isactive to check if the moster has been slaced succesfully 
+        if (IsActive)
+        {
+            // set the position of monster from start point to destination point with specific speed 
+            transform.position = Vector2.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+
+            if (transform.position == destination)
+            {
+                if (path != null && path.Count > 0)
+                {
+                    Animate(GridPosition, path.Peek().Gridposition);
+                    // we just wannna use it's node and get the Gridposition
+
+                    //used to retrieve or fetch the first element of the Stack or the element present at the top of the Stack.
+                    //The element retrieved does not get deleted or removed from the Stack.
+                    GridPosition = path.Peek().Gridposition;
+                    //The element is popped from the top of the stack and is removed from the same.
+                    destination = path.Pop().WorldPosition;
+                }
+            }
+        }
+       
+    }
+
+    private void SetPath(Stack<Node> newPath)
+    {
+        if (newPath != null)
+        {
+            this.path = newPath;
+            Animate(GridPosition, path.Peek().Gridposition);
+            //used to retrieve or fetch the first element of the Stack or the element present at the top of the Stack.
+            //The element retrieved does not get deleted or removed from the Stack.
+            GridPosition = path.Peek().Gridposition;
+            //The element is popped from the top of the stack and is removed from the same.
+            destination = path.Pop().WorldPosition;
+        }
+    }
+
+    private void Animate(Point currentPos, Point newPos)
+    {
+        if (currentPos.Y > newPos.Y)
+        {
+            // moving down
+            myAnimator.SetInteger("Horizontal", 0);
+            myAnimator.SetInteger("Vertical", 1);
+        }
+        else if (currentPos.Y < newPos.Y)
+        {
+            //moving up
+            myAnimator.SetInteger("Horizontal", 0);
+            myAnimator.SetInteger("Vertical", -1);
+        }
+        if (currentPos.Y == newPos.Y)
+        {
+            if (currentPos.X > newPos.X)
+            {
+                //moving left
+                myAnimator.SetInteger("Horizontal", -1);
+                myAnimator.SetInteger("Vertical", 0);
+            }
+            else if (currentPos.X < newPos.X)
+            {
+                //moving right
+                myAnimator.SetInteger("Horizontal", 1);
+                myAnimator.SetInteger("Vertical", 0);
+            }
+        }
+    }
+
+    // if the mosnter hit the protal with tag "RedPortal"
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "RedPortal")
+        {
+            // pass true and release the monster 
+            StartCoroutine(Scale(new Vector3(1, 1), new Vector3(0.1f, 0.1f),true));
+        }
+        if (other.tag == "Tile")
+        {
+            spriteRenderer.sortingOrder = other.GetComponent<TileScript>().GridPosition.Y;
+        }
+    }
+
+    public void Release()
+    {
+        
+        IsActive = false;
+        //set correct start position
+        GridPosition = LevelManager.Instance.BlueSpawn;
+        //remove monster from the game
+        GameManager.Instance.Pool.ReleaseObject(gameObject);
+        //Releases the object in the object pool
+        GameManager.Instance.Removemonster(this);
+    }
+    //taking damage
+    public void TakeDamage(int damage)
+    {
+        if (IsActive)
+        {
+            health.CurrentValue -= damage;
+            if(health.CurrentValue <= 0)
+            {
+                GameManager.Instance.Gold += 2;
+                // go to death state
+                myAnimator.SetTrigger("Die");
+                IsActive = false;
+                GetComponent<SpriteRenderer>().sortingOrder--;
+            }
+        }
+        
+    }
+}
